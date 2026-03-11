f22915a1807c9c0baaf5b0e808a71777bda564a0
Phat Dat Nguyen <nguyenpdat782004@gmail.com>
2026-03-11 18:21:16 +0700

    user address models

---
+++ src/models/userAddress.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userAddressSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    street:{
+        type: String,
+        required: true
+    },
+    city:{
+        type: String,
+        required: true
+    },
+    district:{
+        type: String,
+        required: true
+    },
+    country:{
+        type: String,
+        required: true
+    },
+    isDefault:{
+        type: Boolean,
+        default: false
+    }
+})
+
+export default mongoose.model("UserAddress", userAddressSchema);    
